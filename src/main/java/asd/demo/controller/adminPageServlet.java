07a645b13d3896d1a8fb749192c2e3fa048b2d14
Mougi <mougiosalex@gmail.com>
2019-09-30 15:17:00 +1000

    test

--- src/main/java/asd/demo/controller/adminPageServlet.java
+++ src/main/java/asd/demo/controller/adminPageServlet.java
@@ -22,7 +22,7 @@ import javax.servlet.http.HttpSession;
 
 @WebServlet("./adminServ")
 public class adminPageServlet extends HttpServlet {
-   public void doPost(HttpServletRequest request, HttpServletResponse response)  throws ServletException, IOException{
+   public void doGet(HttpServletRequest request, HttpServletResponse response)  throws ServletException, IOException{
        request.getRequestDispatcher("adminPage.jsp").forward(request, response);
    }
 }
